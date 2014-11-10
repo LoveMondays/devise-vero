24788c370648c9bb6690b80e68930673f47743d1
Estevão Mascarenhas <estevao.am@gmail.com>
2014-11-10 14:24:03 -0200

    Renaming generators

---
+++ lib/generators/devise_vero/install_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators/base'
+
+module Devise
+  module Vero
+    module Generators
+      # InstallGenerator create the conf to Devise integration with Vero
+      class InstallGenerator < Rails::Generators::Base
+        source_root File.expand_path('../templates', __FILE__)
+
+        desc 'Creates the default configuration file for Devise::Vero'
+
+        def copy_initializer
+          template 'devise_vero.rb', 'config/initializers/devise_vero.rb'
+        end
+      end
+    end
+  end
+end
